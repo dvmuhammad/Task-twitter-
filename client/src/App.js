78c0ff6f2f943320effaf196e75d345362a4fa20
Muhammad <78048756+dvmuhammad@users.noreply.github.com>
2021-09-21 19:24:12 +0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 
+const PostForm = () => {
+
+    const [name, setName] = useState('');
+    const [content, setContent] = useState('');
+
+    const postData = (e) => {
+        
+        fetch('http://localhost:8080/tweets', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({text: content, name, username: name})
+        })
+        .catch(err => console.log(err))
+    }
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        if (name === 'name') {
+            setName(value);
+        } else {
+            setContent(value);
+        }
+    }
+
+    return (
+        <div>
+            <h1>Tweet</h1>
+            <form onSubmit={postData}>
+                <div>
+                    <input type="text" name="name" onChange={handleChange} />                
+                    <input type="text" name="text" onChange={handleChange} />                
+                </div>
+                <input type="submit" value="post" onClick={postData} />
+            </form>
+        </div>
+    )
+}
+
 function App() {
   const [items, setItems] = useState([]);
 
@@ -19,17 +59,17 @@ function App() {
 
   return (
     <div>
-      <h1>Twitter</h1>
+      <PostForm />
       <div className="ava">
 
       </div>
       {items.map((post) => (
-        <div class="card">
-          <div class="ovelay"> </div>
-          <header class="user">
+        <div key={post.id} className="card">
+          <div className="ovelay"> </div>
+          <header className="user">
             <img src={post.url} alt="" />
-            <div class="user-info">
-              <h2 class="user-info-name">{post.name}</h2>
+            <div className="user-info">
+              <h2 className="user-info-name">{post.name}</h2>
             </div>
           </header>
           <main>
